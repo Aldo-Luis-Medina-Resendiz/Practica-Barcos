b46bada7a99d653046875d3bd2d37e1fa8633e73
Aldo Luis Medina Resendiz <114273396+Aldo-Luis-Medina-Resendiz@users.noreply.github.com>
2023-01-06 22:09:11 +0100

    Proyect finished
    Adding all the documents, and adding functionality to the server for posting

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 const express = require("express");
 const app = express();
-
 const path = require("path");
 const router = express.Router();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
 
-
 router.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname + "/templates/index.html"))
-})
+	res.sendFile(path.join(__dirname + "/templates/index.html"));
+});
+
+router.get("/contacto", (req, res) => {
+	res.sendFile(path.join(__dirname + "/templates/contacto.html"));
+});
+
+router.get("/perfil", (req, res) => {
+	res.sendFile(path.join(__dirname + "/templates/perfil.html"));
+});
+
+router.post("/", (req, res) => {
+	res.sendFile(path.join(__dirname + "/templates/doc.xslx"));
+});
 
 app.use("/", router);
 app.listen(process.env.port || 8080);
 
-console.log("Servidor abierto en el puerto 8080")
-
-//Minuto 46
+console.log("Servidor abierto en el puerto 8080");
